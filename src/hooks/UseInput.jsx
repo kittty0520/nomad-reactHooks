b6a6b94f6045b4b13e1aebe1a61efbe7fd6985c5
kittty0520 <kittty0520>
2023-06-08 10:47:48 +0900

    Make useInput and UseTabs

---
+++ src/hooks/UseInput.jsx
@@ -0,0 +1,30 @@
+import './styles.css';
+import { useState } from 'react';
+
+const useInput = (initialValue, validator) => {
+	const [value, setValue] = useState(initialValue);
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+		let willUpdate = true;
+		if (typeof validator === 'function') {
+			willUpdate = validator(value);
+		}
+		if (willUpdate) {
+			setValue(value);
+		}
+	};
+	return { value, onChange };
+};
+
+export default function UseInput() {
+	const maxLen = (value) => value.length <= 10; //검증기준을 가지는 함수
+	const name = useInput('Mr.', maxLen);
+	return (
+		<div>
+			<h1>Hello Hooks</h1>
+			<input placeholder='Name' value={name.value} onChange={name.onChange} />
+		</div>
+	);
+}
---
+++ src/hooks/UseTabs.jsx
@@ -0,0 +1,38 @@
+import './styles.css';
+import { useState } from 'react';
+
+const content = [
+	{ tab: 'Section 1', content: "I'm the content of the Section 1" },
+	{
+		tab: 'Section 2',
+		content: "I'm the content of the Section 2",
+	},
+];
+
+const useTabs = (initialTab, allTabs) => {
+	const [currentIndex, setCurrentIndex] = useState(initialTab);
+	if (!allTabs || !Array.isArray(allTabs)) return;
+
+	const changeItem = (currentIndex) => setCurrentIndex(currentIndex);
+	const currentItem = allTabs[currentIndex];
+	return { currentItem, changeItem };
+
+	// return {
+	//   currentItem: allTabs[currentIndex],
+	//   changeItem: setCurrentIndex
+	// };
+};
+
+export default function UseTabs() {
+	const { currentItem, changeItem } = useTabs(0, content);
+	// console.log(tabs);
+	return (
+		<div>
+			<h1>Hello Hooks</h1>
+			{content.map((section, index) => (
+				<button onClick={() => changeItem(index)}>{section.tab}</button>
+			))}
+			<div>{currentItem.content}</div>
+		</div>
+	);
+}
