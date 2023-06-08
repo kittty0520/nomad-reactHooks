d61c5210003e20f7656bf176fdfe8adc36cc106a
kittty0520 <kittty0520>
2023-06-08 12:41:06 +0900

    Make UseTitle

---
+++ src/hooks components/UseTitle.jsx
@@ -0,0 +1,23 @@
+import './styles.css';
+import { useEffect, useState } from 'react';
+
+const useTitle = (initialTitle) => {
+	const [title, setTitle] = useState(initialTitle);
+	const updateTitle = () => {
+		const htmlTitle = document.querySelector('title');
+		htmlTitle.innerText = title;
+	};
+	useEffect(updateTitle, [title]);
+
+	return setTitle;
+};
+
+export default function UseTitle() {
+	const titleUpdator = useTitle('Loading...');
+	setTimeout(() => titleUpdator('home'), 3000);
+	return (
+		<div>
+			<h1>Hello Hooks</h1>
+		</div>
+	);
+}
