393855a77dc813cf51bce7d4bf83fe53dfc45c36
kittty0520 <kittty0520>
2023-06-10 00:44:35 +0900

    Change confirm to window.confirm

--- src/hooks components/UseConfirm.jsx
+++ src/hooks components/UseConfirm.jsx
@@ -2,7 +2,7 @@ const useConfirm = (message, onConfirm, onCancel) => {
 	if (!onConfirm || typeof onConfirm !== 'function') return;
 	if (onCancel && typeof onCancel !== 'function') return;
 	const confirmAction = () => {
-		if (confirm(message)) {
+		if (window.confirm(message)) {
 			onConfirm();
 		} else {
 			onCancel();
