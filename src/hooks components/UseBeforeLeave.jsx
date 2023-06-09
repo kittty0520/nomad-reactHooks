41e0dd287add6b5f365f91eb5f8e39aaaad5ee90
kittty0520 <kittty0520>
2023-06-10 00:44:12 +0900

    Make useBeforeLeave

---
+++ src/hooks components/UseBeforeLeave.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+const useBeforeLeave = (onBefore) => {
+	const handle = (event) => {
+		const { clientY } = event;
+		//clientY : 마우스 위치의 y값
+		//마우스가 화면 위로 벗어나면 ClientY값이 0보다 작아지는 것을 이용
+		if (clientY <= 0) {
+			onBefore();
+		}
+	};
+	const showMouseLeave = () => {
+		document.addEventListener('mouseleave', handle);
+		return () => document.removeEventListener('mouseleave', handle);
+	};
+	useEffect(showMouseLeave);
+	if (typeof onBefore !== 'function') return;
+};
+
+export default function UseBeforeLeave() {
+	const begForLife = () => console.log("please don't leave");
+	useBeforeLeave(begForLife);
+	return (
+		<div className='App'>
+			<h1>Hello Hooks</h1>
+		</div>
+	);
+}
